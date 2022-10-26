b7d9c41f5ade668d9f39ed69c9d2311b9962be57
Marcus Vinicius <marcusviniciusfabr@gmail.com>
2022-10-26 20:43:34 -0300

    lesson 3

--- myApp.js
+++ myApp.js
@@ -1,43 +1,28 @@
 let express = require('express');
 let app = express();
 
+/**
+ * lesson 1
+ */
 // app.get('/', function(req, res) {
 //     res.send('Hello Express')
 // })
 
-let absolutePath = __dirname + '/views/index.html'
+/**
+ * lesson 2
+ */
+// let absolutePath = __dirname + '/views/index.html'
+// app.get('/', function(req, res) {
+//     res.sendFile(absolutePath)
+// })
 
-app.get('/', function(req, res) {
-    res.sendFile(absolutePath)
-})
+/**
+ * lesson 3
+ */
+// app.use('/public', express.static(__dirname + '/public'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/views/index.html')
+// })
 
  module.exports = app;
