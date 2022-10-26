3bdcfc16bc583da1061fbec6ac5d1e62264ebec4
Marcus Vinicius <marcusviniciusfabr@gmail.com>
2022-10-26 20:15:15 -0300

    lesson 2

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let app = express();
 
+// app.get('/', function(req, res) {
+//     res.send('Hello Express')
+// })
+
+let absolutePath = __dirname + '/views/index.html'
+
 app.get('/', function(req, res) {
-    res.send('Hello Express')
+    res.sendFile(absolutePath)
 })
 
 
