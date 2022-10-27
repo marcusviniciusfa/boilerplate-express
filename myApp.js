ec9dd13444834cad251503ddd01190a407c0dcd8
Marcus Vinicius <marcusviniciusfabr@gmail.com>
2022-10-26 21:43:43 -0300

    lesson 7

--- myApp.js
+++ myApp.js
@@ -48,13 +48,23 @@ let app = express();
 /**
  * lesson 6
  */
-app.use(function(req, res, next) {
-    console.log(`${req.method} ${req.path} - ${req.ip}`)
-    next()
-})
+// app.use(function(req, res, next) {
+//     console.log(`${req.method} ${req.path} - ${req.ip}`)
+//     next()
+// })
 
-app.get('/', function(req, res) {
-    res.send('Hello Express')
+// app.get('/', function(req, res) {
+//     res.send('Hello Express')
+// })
+
+/**
+ * lesson 7
+ */
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString()
+    next()
+}, function(req, res) {
+    res.json({time: req.time})
 })
 
  module.exports = app;
